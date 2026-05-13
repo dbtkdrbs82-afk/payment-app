1879370fe90d667285a3f67f7990f72bc39ad18b
dbtkdrbs82-afk <dbtkdrbs82@gmail.com>
2026-05-13 16:14:57 +0900

    add success page

--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,24 @@ document.querySelector<HTMLButtonElement>('#pay-button')!.addEventListener('clic
     failUrl: window.location.origin + '/fail',
   })
 })
+const path = window.location.pathname;
+
+if (path === "/success") {
+  const params = new URLSearchParams(window.location.search);
+
+  const orderId = params.get("orderId");
+  const amount = params.get("amount");
+
+  document.querySelector("#app")!.innerHTML = `
+    <div class="page">
+      <div class="payment-card">
+        <h1>결제 성공</h1>
+        <p>주문번호: ${orderId}</p>
+        <p>결제금액: ${amount}원</p>
+        <button onclick="window.location.href='/'">
+          처음으로
+        </button>
+      </div>
+    </div>
+  `;
+}
