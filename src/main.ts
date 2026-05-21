09344f76d62dd69e631f3ab441c7ccadc26ebe4a
dbtkdrbs82-afk <dbtkdrbs82@gmail.com>
2026-05-21 20:52:45 +0900

    improve mobile thank you message layout

--- src/main.ts
+++ src/main.ts
@@ -873,8 +873,8 @@ const { error } = await supabase.from('payments').insert([
       <h1>
   ${
     currentEventType === 'funeral'
-      ? '명복을 빌어 주셔서<br>감사합니다.'
-      : '축하해 주셔서<br>감사합니다 💛'
+      ? '<span style="display:block;">명복을 빌어 주셔서</span><span style="display:block;">감사합니다.</span>'
+      : '<span style="display:block;">축하해 주셔서</span><span style="display:block;">감사합니다</span>'
   }
 </h1>
         <p>주문번호: ${orderId}</p>
