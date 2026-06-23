6385fe466caf7de0c77031f4f728196a9cd2835d
dbtkdrbs82-afk <dbtkdrbs82@gmail.com>
2026-06-23 20:26:21 +0900

    update korpay return redirect

--- api/korpay-return.ts
+++ api/korpay-return.ts
@@ -1,6 +1,8 @@
 export default function handler(req: any, res: any) {
+  const orderNo = req.body?.orderNumber || req.body?.orderNo || req.query?.orderNo || ''
+
   res.writeHead(302, {
-    Location: '/kiosk-success'
+    Location: '/kiosk-success?orderNo=' + encodeURIComponent(orderNo)
   })
   res.end()
 }
