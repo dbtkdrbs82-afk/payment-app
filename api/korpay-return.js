77d4f2bbfa094e76f6a2a84dd8356a5f495aad1d
dbtkdrbs82-afk <dbtkdrbs82@gmail.com>
2026-06-29 19:16:17 +0900

    redirect korpay return to success

--- api/korpay-return.js
+++ api/korpay-return.js
@@ -1,3 +1,22 @@
 export default function handler(req, res) {
-  res.status(200).send('korpay-return api ok')
+  const body = req.body || {}
+
+  const orderId = encodeURIComponent(body.orderNumber || '')
+  const amount = encodeURIComponent(body.amount || '')
+  const paymentKey = encodeURIComponent(body.paymentKey || '')
+  const merchantId = encodeURIComponent(body.reserved || '')
+
+  const redirectUrl =
+    '/success?source=kiosk' +
+    '&orderId=' + orderId +
+    '&amount=' + amount +
+    '&paymentKey=' + paymentKey +
+    '&merchantId=' + merchantId
+
+  res.status(200).send(
+    '<!doctype html><html><head><meta charset="utf-8"></head><body>' +
+    '<script>window.location.href="' + redirectUrl + '";</script>' +
+    '결제 완료 처리 중입니다...' +
+    '</body></html>'
+  )
 }
