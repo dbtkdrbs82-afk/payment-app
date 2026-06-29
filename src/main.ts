9a1b1f03c99711eecef9705c2e698bd7c15b10bb
dbtkdrbs82-afk <dbtkdrbs82@gmail.com>
2026-06-29 17:54:47 +0900

    fix korpay api return redirect

--- src/main.ts
+++ src/main.ts
@@ -9913,7 +9913,7 @@ const paymentData = {
   orderNumber: orderNo.replace(/[^a-zA-Z0-9]/g, ''),
   amount: totalPrice,
   payMethod: 'card',
-  returnUrl: window.location.origin + '/',
+  returnUrl: window.location.origin + '/success',
   ediDate: ediDate,
   hashKey: hashKey,
   customerName: '미니상점 고객',
