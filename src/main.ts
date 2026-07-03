0af4708d7abc8203d5d020fd7014ae544ddac6a1
dbtkdrbs82-afk <dbtkdrbs82@gmail.com>
2026-07-03 18:47:49 +0900

    출금관리 페이지네이션 및 10 20 50 보기 수정

--- src/main.ts
+++ src/main.ts
@@ -5004,19 +5004,25 @@ console.log('저장 error:', error)
           })
       }
     
-      const payoutPageSizeSelect =
-        document.querySelector<HTMLSelectElement>('#withdraw-page-size')
-    
-      if (payoutPageSizeSelect) {
-        payoutPageSizeSelect.value = String(payoutPageSize)
-    
-        payoutPageSizeSelect.addEventListener('change', () => {
-          payoutPageSize = Number(payoutPageSizeSelect.value)
-          payoutPage = 1
-          sessionStorage.setItem('withdraw_page_size', String(payoutPageSize))
-          renderPayoutTable()
-        })
-      }
+      const payoutPageSizeSelects =
+  document.querySelectorAll<HTMLSelectElement>('.withdraw-page-size')
+
+payoutPageSizeSelects.forEach((select) => {
+  select.value = String(payoutPageSize)
+
+  select.addEventListener('change', () => {
+    payoutPageSize = Number(select.value)
+    payoutPage = 1
+
+    sessionStorage.setItem('withdraw_page_size', String(payoutPageSize))
+
+    payoutPageSizeSelects.forEach((otherSelect) => {
+      otherSelect.value = String(payoutPageSize)
+    })
+
+    renderPayoutTable()
+  })
+})
     
       document.querySelector('#payout-search-btn')
         ?.addEventListener('click', () => {
