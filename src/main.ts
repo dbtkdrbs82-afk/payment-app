c5f974654a970909e989e6654639ec627b5e71cf
dbtkdrbs82-afk <dbtkdrbs82@gmail.com>
2026-09-02 20:21:26 +0900

    adjust beauty schedule date buttons position

--- src/main.ts
+++ src/main.ts
@@ -22151,7 +22151,7 @@ if (beautyReservedOrdersError) {
     display:flex;
     align-items:center;
     gap:6px;
-    transform:translateY(-12px);
+    transform:translateY(-18px);
   "
 >
   <button
