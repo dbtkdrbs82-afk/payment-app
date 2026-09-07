2add13fd06f166730cf4d8e578315d6ad704c2d0
dbtkdrbs82-afk <dbtkdrbs82@gmail.com>
2026-09-07 13:58:25 +0900

    adjust head office quick filter button position

--- src/main.ts
+++ src/main.ts
@@ -14484,7 +14484,7 @@ const savedHeadOfficeKeyword =
           type="button"
             style="
   position:relative;
-  top:-9px;
+  top:-10px;
 "
         >
           이전
@@ -14497,7 +14497,7 @@ const savedHeadOfficeKeyword =
           type="button"
             style="
   position:relative;
-  top:-9px;
+  top:-10px;
 "
         >
           오늘
@@ -14511,7 +14511,7 @@ const savedHeadOfficeKeyword =
           type="button"
             style="
   position:relative;
-  top:-9px;
+  top:-10px;
 "
         >
           다음
@@ -14524,7 +14524,7 @@ const savedHeadOfficeKeyword =
           type="button"
             style="
   position:relative;
-  top:-9px;
+  top:-10px;
 "
         >
           당월
